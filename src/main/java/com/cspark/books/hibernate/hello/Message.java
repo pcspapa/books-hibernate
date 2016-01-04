58502fb1eb96ca9491767a0adb88ed481db49ded
pcspapa <pcspapa@gmail.com>
2016-01-04 15:45:17 +0900

    도메인 모델 만들기

---
+++ src/main/java/com/cspark/books/hibernate/hello/Message.java
@@ -0,0 +1,44 @@
+package com.cspark.books.hibernate.hello;
+
+/**
+ * Created by cspark on 2016. 1. 4..
+ */
+public class Message {
+
+    private Long id;
+
+    private String text;
+
+    private Message nextMessage;
+
+    public Message() {
+    }
+
+    public Message(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Message getNextMessage() {
+        return nextMessage;
+    }
+
+    public void setNextMessage(Message nextMessage) {
+        this.nextMessage = nextMessage;
+    }
+}
